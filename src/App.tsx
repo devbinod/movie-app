3306bd8e6e3f84a840bae126b2d31e0b77ec60ea
binodpant <binodpant.nep@gmail.com>
2020-01-19 09:44:21 +0545

    balaa balla

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,34 @@ import "./App.css";
 import Movie from "./components/movies/index";
 import { Provider } from "react-redux";
 import store from "./store";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MovieDetail from "./components/movies/movieDetail";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green
+  },
+  spacing: 4
+});
 const App: React.FC = () => {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <Movie />
-      </div>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Switch>
+              <Route exacet path="/movie" component={Movie} />
+              <Route exacet path="/" component={Movie} />
+              <Route exacet path="/movie/:id/detail" component={MovieDetail} />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
+    </ThemeProvider>
   );
 };
 
---
+++ src/components/movies/movieDetail.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const MovieDetail = () => {
+  return <div>Movie Detail.............</div>;
+};
+
+export default MovieDetail;
